604f9abf4863a7fbe156bab020f566401ccc5244
daftboston <dsantoyop@gmail.com>
2022-12-15 15:45:45 -0500

    condicionales

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,72 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import UserCard from './components/UserCard';
-import ListGroceries from './components/ListGroceries';
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <h1>Mi primer proyecto de react</h1>
-      {/*
-      generar un pequeño contenedor /card
-      titulo su nombre
-      ua lista con :
-      pelicula favorita
-      banda favorita  */}
-      <UserCard
-      name = "Daniel"
-      movie = "Avatar"
-      band = "Stone Temple Pilots"/>
-      <UserCard
-      name = "jerry"
-      movie = "cars"
-      band = "fnaf"      
-      />
-      <UserCard
-       name = "dely"
-       movie = "bots"
-       band = "toto"      
-      />
-
-      <ListGroceries
-      name= "Leche"
-      quant= "2"
-      price= "3.00"      
-      />
-
-     <ListGroceries
-      name= "Pan"
-      quant= "10"
-      price= "0.50"      
-      />
+  /* 
+  if (100>200) {
+     return (
+      <div className='App'>
+        <h1>La condicion se cumplio</h1>
+      </div>
+     )
+    
+  } else {
+    return(
+    <div className='App'>
+        <h1>La condicion NO se cumple</h1>
+    </div>)
+  } 
+  */
 
 
-     <ListGroceries
-      name= "Huevos"
-      quant= "2"
-      price= "0.50"      
-      />
 
-     <ListGroceries
-      name= "Queso"
-      quant= "1"
-      price= "3.00"      
-      />
+return (
+  <div className='App'>
+    {
+      /*dentro del jsx solo funciona el: Operador tenario, y operadores corto circuito*/
 
+      /*condicion ? se devuelve si la condicion se cumbple : si la condicion no se cumple 
+      
+      TRUTHYS--> Se comportan como boleanos true
+
+      []-->arreglos vacios
+
+      {}--> objetos vacios
+
+      -200 --> numeros negativos
+
+      "False"--> la palabra false
+
+      " "--> cadena de texto que tenga vacios
+
+
+
+
+      FALSIES--> Se comportan como boleanos false
+      ""-->cadena de texto sin vacios
+      undefined
+
+      null
+
+      NaN
+
+      0
+
+      False
+      
+      */
+      }
+      {
+        100!==200 ? <h1>  la condicion se cumple</h1> : <h1> la condicion no se cumple</h1>
+      }
     
 
-      
-        
-    </div>
-  )
-}
+    
+  </div>
+)
+    }
 
 export default App
