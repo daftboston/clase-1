bf8b088ff49a24fe82c3f36f88ff50fd0a1ce16a
daftboston <dsantoyop@gmail.com>
2023-01-11 09:34:43 -0500

    clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,55 @@
 
-import './App.css'
-import axios from 'axios'
-import {useState, useEffect} from 'react'
-
-import FormComponent from './components/FormComponent'
-import FormUser from './components/FormUser'
-import  ToDoForm from './components/ToDoForm'
-import UsersList from './components/UsersList'
+import { useState } from "react";
+import FormUser from "./components/FormUser";
+import usersData from "./data/userData.js";
+import UsersList from "./components/UsersList";
 
 function App() {
-  
+  const [users, setUsers] = useState(usersData);
+  const [userDataUpdate, setUserDataUpdate] = useState(null);
+  //posibles valores
+  //null -> cuando no haya nada seleccionado
+  //{ name, id... } -> hay un usuario seleccionado
+
+  const addUser = (data) => {
+    setUsers([...users, data]);
+  };
+
+  const deleteUser = (userId) => {
+    //Filtrar a todos los usuarios diferentes al usuario que queremos eliminar
+    //Todos los usuarios que queremos conservar
+
+    const filteredUsers = users.filter((user) => user.id !== userId);
+
+    setUsers(filteredUsers);
+  };
+
+  //Estados no deben mutarse
+
+  const selectUser = (userData) => {
+    setUserDataUpdate(userData);
+  };
+
+  const updateUser = (editedUser) => {
+    const index = users.findIndex((user) => user.id === editedUser.id);
+
+    users[index] = editedUser;
+    setUsers([...users]);
+    setUserDataUpdate(null);
+  };
 
   return (
-    <div className="App">      
-      <FormUser></FormUser>
-      <hr />
-      <ToDoForm></ToDoForm>
-      <UsersList></UsersList>
-
-       
+    <div className="App">
+      <FormUser
+        createUserData={(data) => addUser(data)}
+        userSelectedData={userDataUpdate}
+        updateUser={updateUser}
+      />
+      <UsersList
+        users={users}
+        deleteUser={deleteUser}
+        selectUser={selectUser}
+      />
     </div>
   );
 }
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,18 +1,25 @@
-const UsersList = () => {
-    return (
 
+const UsersList = ({users, deleteUser}) => {
+
+
+    return(
         <ul>
             {
-                users?.map( (userElement, index) => ()
-            <li>
-                <h4><span>Nombre</span>Nombre</h4>
-                <h4><span>Email</span>email</h4>
-                <h4><span>Edad</span>xx</h4>
-                <h4><span>Vacunacion</span>False</h4>
-            </li>
+                users?.map( (userElement, index) => (
+                    <li key={`user-${index}`}>
+                        <h4><span>Nombre:</span> {userElement.name}</h4>
+                        <h4><span>Email:</span> {userElement.email}</h4>
+                        <h4><span>Edad:</span> {userElement.age}</h4>
+                        <h4><span>Vacunacion:</span> {userElement.isVaccinated.toString()}</h4>
+                        <h4><span>Genero:</span> {userElement.gender}</h4>
+
+                        <button onClick={ () => deleteUser(userElement.id) }>Eliminar</button>
+                    </li>
+                ) )
             }
         </ul>
     )
+
 }
 
 export default UsersList
